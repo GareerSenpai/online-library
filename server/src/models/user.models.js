9ce4af2d2fa922f6e76926be17562ea78bff80b8
Anurag Bhattacharjee <anuragbhattacharjee612@gmail.com>
2024-04-12 21:49:31 +0530

    install and use package bcrypt and jsonwebtoken hash password if password is modified using bcrypt create isPasswordCorrect method in userSchema using bcrypt create generateAccessToken and generateRefreshToken in userSchema usign jwt

--- server/src/models/user.models.js
+++ server/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -70,9 +72,52 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "Free",
     },
+    refreshToken: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
 // timestamps will give us createdAt and updatedAt
+
+// so that user saves password in encrypted form whenever only password is modified
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// to be used when user wants to login
+userSchema.methods.isPasswordCorrect = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
